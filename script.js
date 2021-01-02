f8c939613823d735c290596a880ea5c9fad374e1
KenzieAlyce <73435380+KenzieAlyce@users.noreply.github.com>
2021-01-02 15:56:07 -0600

    update

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+function startFunction (block:display) 
 
+button.addEventListener ("click", startFunction); 
+
+var count = 0;
+var start = document.getElementById ("start");
+var Q1 = document.getElementById ("Q1");
+var Q2 = document.getElementById ("Q2");
+var Q3 = document.getElementById ("Q3");
+var Q4 = document.getElementById ("Q4");
+var Q5 = document.getElementById ("Q5");
+var end = document.GetElementById ("end");
+var score = document.GetElementById ("score");
+var quizOrder = ["start", "Q1", "Q2", "Q3", "Q4", "Q5", "end", "score";]
+ 
+
+addEventListener.button ("click", if 
+
+"correct"
+
+count +1
+
+say "Correct!"
+
+else 
+
+say "Wrong!"
+
+then
+
+quizOrder +1 //?????
+);
