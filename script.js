f67eefc13904c00662cd72b8e62ef49a0e62dfe9
KenzieAlyce <73435380+KenzieAlyce@users.noreply.github.com>
2021-01-07 14:38:38 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 
 var questionIndex = 0;
-var answers = document.querySelectorAll('.answer');
+
 var welcomeSec = document.querySelector('#welcomeSection');
 var questionOne = document.querySelector('#questionOne');
+var questionTwo = document.querySelector ('#questionTwo');
+var questionThree = document.querySelector ('#questionThree');
+var questionFour = document.querySelector ('questionFour');
+var questionFive = document.querySelector ('questionFive');
 var startBtn = document.querySelector('#startButton');
 
 
 
-startBtn.addEventListener("click", startQuiz),
+startBtn.addEventListener("click", startQuiz)
+  
 function startQuiz() {
 welcomeSec.classList.add('hidden'); // hides the Welcome section
 questionOne.classList.remove('hidden'); // display/shows the Quiz section
